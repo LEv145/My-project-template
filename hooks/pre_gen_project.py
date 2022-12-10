c6633229a2dd188d7f31ac7726763bc3b3d51631
LEv145 <you@example.com>
2022-12-11 01:30:28 +0200

    v 0.2.2
    Add hooks

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,14 @@
+import re
+import sys
+
+
+MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+MODULE_NAME = "{{cookiecutter.project_slug}}"
+
+
+if not re.match(MODULE_REGEX, MODULE_NAME):
+    print(
+        f"ERROR: The project slug ({MODULE_NAME}) is not a valid Python module name."
+        " Please do not use \"-\" and use \"_\" instead"
+    )
+    sys.exit(1)
